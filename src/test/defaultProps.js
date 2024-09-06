c7dbd28ca1fe0587e2d7f0d650173107e80d9110
Curryyleaf <166195603+Curryyleaf@users.noreply.github.com>
2024-09-06 07:09:33 -0700

    checkouts

---
+++ src/test/defaultProps.js
@@ -0,0 +1,61 @@
+import { vi } from "vitest";
+
+const defaultProps = {
+  tableHeadConfig: [],
+
+  products: [],
+  visibleColumns: [],
+  PriceBracket: [],
+  togglePrice: vi.fn(),
+  closePriceToggle: vi.fn(),
+
+  toggleDropDown: vi.fn(),
+  isAllSelected: false,
+  toggleAllSelect: vi.fn(),
+  selectItem: vi.fn(),
+  sorting: vi.fn(),
+  deleteProduct: vi.fn(),
+  groupProductsByPriceBracket: vi.fn(),
+  closeDropdown: vi.fn(),
+};
+
+export default defaultProps;
+
+// let togglePrice;
+// let toggleDropDown;
+// let toggleAllSelect;
+// let selectItem;
+// let sorting;
+// let deleteProduct;
+// let groupProductsByPriceBracket;
+// let closeDropdown;
+// let closePriceToggle;
+
+export const myMethods = [
+  "togglePrice",
+  "closePriceToggle",
+  "toggleDropDown",
+  // "toggleAllSelect",
+  "selectItem",
+  "sorting",
+  "deleteProduct",
+  "groupProductsByPriceBracket",
+  "closeDropdown",
+];
+
+export const incorrectProps = {
+  togglePrice: vi.fn(),
+  closePriceToggle: vi.fn(),
+  toggleDropDown: vi.fn(),
+  isAllSelected: false,
+  toggleAllSelect: vi.fn(),
+  selectItem: vi.fn(),
+  sorting: vi.fn(),
+  deleteProduct: vi.fn(),
+  groupProductsByPriceBracket: vi.fn(),
+  closeDropdown: vi.fn(),
+  product: {},
+  tableHeadConfig: [],
+  visibleColumns: {},
+  sorting: [],
+};
